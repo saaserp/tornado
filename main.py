c316b61a7aa9cef84b512f29916775cf94d193eb
liuwencai <821272852@qq.com>
2016-01-27 23:27:03 +0800

    templates and forms

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
@@ -9,16 +10,19 @@ define("port",default=8000,type = int)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-    	arg = self.get_argument('q','hello')
-        self.write("Hello, world"+arg)
+        self.write('index.html')
 
-application = tornado.web.Application([
-    (r"/", MainHandler),
-])
+class ContentHandler(tornado.web.RequestHandler):
+    def post(self):
+	n1 = self.get_argument('n1')
+	n2 = self.get_argument('n2')
+	n3 = self.get_argument('n3')
+	self.render('content.html',n1=n1,n2=n2,n3=n3)
+
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
-    app = tornado.web.Application(MainHandlers=[(r"/",MainHandler)])
+    app = tornado.web.Application(MainHandlers=[(r"/",MainHandler),(r'/content',ContentHandler)],template_path=os.path.join(os.path.dirname(__file__),'templates'))
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
