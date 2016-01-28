926ffc0a6d6df1adc75c1b42923cba0cd182bd66
liuwencai <821272852@qq.com>
2016-01-28 15:11:54 +0800

    modify some code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tornado
 tornado framework
+#how to run it?
+shell:sudo python main.py --port=80
--- main.py
+++ main.py
@@ -1,28 +1,33 @@
-import os
+import os.path
+
 import tornado.httpserver
 import tornado.ioloop
-import tornado.web
 import tornado.options
+import tornado.web
 
-from tornado.options import define,options
+from tornado.options import define, options
+define("port", default=8000, help="run on the given port", type=int)
 
-define("port",default=8000,type = int)
-
-class MainHandler(tornado.web.RequestHandler):
+class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write('index.html')
+        self.render('index.html')
 
 class ContentHandler(tornado.web.RequestHandler):
     def post(self):
-	n1 = self.get_argument('n1')
-	n2 = self.get_argument('n2')
-	n3 = self.get_argument('n3')
-	self.render('content.html',n1=n1,n2=n2,n3=n3)
+        n1 = self.get_argument('n1')
+        n2 = self.get_argument('n2')
+        n3 = self.get_argument('n3')
+        self.render('content.html', n1=n1, n2=n2, n3=n3)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     tornado.options.parse_command_line()
-    app = tornado.web.Application(MainHandlers=[(r"/",MainHandler),(r'/content',ContentHandler)],template_path=os.path.join(os.path.dirname(__file__),'templates'))
+    app = tornado.web.Application(
+            handlers=[(r'/', IndexHandler), (r'/content', ContentHandler)],
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path = os.path.join(os.path.dirname(__file__), "static")
+            )
+
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
